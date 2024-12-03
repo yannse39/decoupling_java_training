d5a29213c92e49cdc264b0c3a1ed8ee637e15400
yannse667 <mezayann92i@gmail.com>
2024-12-03 20:04:48 +0100

    Partie 5 - Composition de plusieurs Loggers

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -3,8 +3,12 @@ package fr.lernejo.logger;
 public class LoggerFactory {
 
     public static Logger getLogger(String name) {
+        Logger consoleLogger = new ConsoleLogger();
         Logger fileLogger = new FileLogger("target/captain.log");
-        Logger contextualLogger = new ContextualLogger(name, fileLogger);
-        return contextualLogger;
+
+        Logger contextualConsoleLogger = new ContextualLogger(name, consoleLogger);
+        Logger contextualFileLogger = new ContextualLogger(name, fileLogger);
+
+        return new CompositeLogger(contextualConsoleLogger, contextualFileLogger);
     }
 }
